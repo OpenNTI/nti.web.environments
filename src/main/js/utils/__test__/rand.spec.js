aca773e4f3b5f7326846b4c308401b62a3026a2a
Jonathan Troyer <jonathan.troyer@nextthought.com>
2020-01-17 11:17:45 -0600

    Add a function to generate a random number of a certain length

---
+++ src/main/js/utils/__test__/rand.spec.js
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+import * as Rand from '../rand.js';
+
+describe('randomNumberOfLength', () => {
+	test ('to have the correct length', () => {
+		const length = 6;
+		expect(Rand.randomNumberOfLength(length)).toHaveLength(length);
+	});
+});
---
+++ src/main/js/utils/rand.js
@@ -0,0 +1,21 @@
+const digits = [0,1,2,3,4,5,6,7,8,9];
+
+/**
+ * Generates a random number of a specified length, avoiding offensive numbers
+ * @param {number} length the length of the number generated
+ * @return {string} a random number
+ */
+export function randomNumberOfLength (length) {
+	let availableDigits = digits.slice();
+	let number = '';
+	for(let i = 0; i < length; i++) {
+		let randDigitIndex = Math.floor(Math.random() * availableDigits.length);
+		number += availableDigits[randDigitIndex];
+		availableDigits.splice(randDigitIndex, 1);
+		if(availableDigits.length <= 0) {
+			availableDigits = digits.slice();
+		}
+	}
+
+	return number;
+}

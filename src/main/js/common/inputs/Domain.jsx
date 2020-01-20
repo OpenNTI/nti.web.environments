7518829055924573ebd2193a6f08600942b8ee2c
Jonathan Troyer <jonathan.troyer@nextthought.com>
2020-01-20 10:45:23 -0600

    Add reserved space attribute

--- src/main/js/common/inputs/Domain.jsx
+++ src/main/js/common/inputs/Domain.jsx
@@ -3,7 +3,7 @@ import {Form} from '@nti/web-commons';
 
 import {Domain}	from '../../utils/index';
 
-export default function DomainInput (props) {
+export default function DomainInput (reservedSpace = 0, props) {
 	const [domain, setDomain] = React.useState('');
 
 	return (
@@ -12,7 +12,7 @@ export default function DomainInput (props) {
 			value={domain}
 			onChange={value => setDomain(Domain.massageToDomain(value))}
 			pattern="[a-z]([a-z0-9-]*?[a-z0-9])*?"
-			maxLength="63"
+			maxLength={63 - reservedSpace}
 		/>
 	);
 }

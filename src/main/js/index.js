6570f6966884f543bd9377fb3eae78be7a6bbee0
Jonathan Grimes <jsg2021@gmail.com>
2021-02-16 17:57:03 -0600

    add error reporter

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initErrorReporter } from '@nti/web-client';
 
 import App from './app';
+
+initErrorReporter();
 
 ReactDOM.render(
 	React.createElement(App),
